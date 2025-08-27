4255e443db826ee4420d168caa50a4c5df8306e2
miguelhdz444 <jrincon35@ucol.mx>
2025-08-27 11:16:16 -0600

    borre

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<?php include("includes/header.php"); ?>
-  
-  <section class="hero">
-    <h2 class="titulo-pedido">¿Cómo quieres tu pedido hoy?</h2>
-    <div class="botones">
-      <a href="menu.php" class="boton">Pickup</a>
-      <a href="menu.php" class="boton">Delivery</a>
-    </div>
-  </section>
 
-  <section class="categorias">
-    <div class="categoria">
-      <i class="fa-solid fa-bowl-rice"></i>
-      <a href="menu.php">Pickup</a>
-    </div>
-    <div class="categoria">
-      <i class="fa-solid fa-utensils"></i>
-      <p>Noodles</p>
-    </div>
-    <div class="categoria">
-      <i class="fa-solid fa-egg"></i>
-      <p>Rollos</p>
-    </div>
-    <div class="categoria">
-      <i class="fa-solid fa-mug-hot"></i>
-      <p>Sopas</p>
-    </div>
-    <div class="categoria">
-      <i class="fa-solid fa-leaf"></i>
-      <p>Té</p>
-    </div>
-  </section>
 
 <?php include("includes/footer.php"); ?>
