49ff191a70070f8497c26a4722a183eb2f14c779
friedman <amariz@ucol.mx>
2025-08-27 12:23:54 -0600

    prueba

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Comida China Express</title>
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-</head>
-<body>
-  <header>
-    <h1>Comida China Express</h1>
-    <nav>
-      <a href="index.php">Inicio</a>
-      <a href="menu.php">Menú</a>
-      <a href="contacto.php">Contacto</a>
-    </nav>
-  </header>
-
-  <main> <!-- 👈 abrimos main aquí -->
+<header>
+  <div class="header-container">
+    <a href="index.php" class="logo-link">
+      <img src="TRABAJO PI/Imagenes/logo comida.png" alt="Logo Comida China Express" class="logo">
+    </a>
+    <nav>
+      <a href="index.php">Inicio</a>
+      <a href="menu.php">Menú</a>
+      <a href="contacto.php">Contacto</a>
+    </nav>
+  </div>
+</header>
